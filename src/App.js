d89863974008b1fad6805c39993dea00c1d790c8
Devendra Sharma <dev73511@gmail.com>
2024-01-08 20:37:09 +0530

    module import updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './assets/css/root.css';
 import Home from './pages/home';
 
